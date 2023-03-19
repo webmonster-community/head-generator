c5af06fa1063e053068a27e4c5e2f6359bb93891
WemonsterA <hello@webmonster.tech>
2023-03-19 11:57:02 -0400

    Update HeadGenerator.php

--- src/HeadGenerator.php
+++ src/HeadGenerator.php
@@ -2,31 +2,6 @@
 
 namespace webmonsterSEO;
 
-/**
- *
- *  $head = new HeadGenerator();
-    $head->setLanguage('fr');
-    $head->setViewport('width=device-width, initial-scale=1');
-    $head->setTitle("Titre de la page - Mots clés importants | Nom de la marque");
-    $head->setDescription("Description pertinente de la page en moins de 160 caractères");
-    $head->addMeta('keywords', 'Mots clés importants séparés par des virgules');
-    $head->setAuthor("Nom de l'auteur ou de l'entreprise");
-    $head->setCanonicalUrl('https://www.example.com/canonical-url');
-    $head->addMeta('robots', 'noindex, nofollow');
-
-    // add CSS links
-    $head->addStyleSheetUrl('https://www.example.com/css/style.css');
-    $head->addStyleSheetUrl('https://www.example.com/css/theme.css');
-
-    // add JS links
-    $head->addScriptUrl('https://www.example.com/js/script.js');
-    $head->addScriptUrl('https://www.example.com/js/plugins.js');
-    $head->render();
- *
- *
- */
-
-
 interface HeadGeneratorInterface {
 
     public function setLanguage(string $language): void;
