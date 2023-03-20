be878bcf597b0c0ac5c11c18af44466ce1d8af8c
WemonsterA <hello@webmonster.tech>
2023-03-20 12:01:22 -0400

    v0.0.7

--- src/HeadGenerator.php
+++ src/HeadGenerator.php
@@ -1,32 +1,6 @@
 <?php
 namespace webmonsterSEO;
 
-interface HeadGeneratorInterface {
-
-    public function setLanguage(string $language);
-    public function setViewport(string $viewport);
-    public function setTitle(string $title);
-    public function setDescription(string $description);
-    public function setKeywords(string $keywords);
-    public function setAuthor(string $author);
-    public function setRobots(string $robots);
-    public function setCreationDate(string $creationDate);
-    public function setLastModified(string $lastModified);
-    public function setGeoPosition(string $geoPosition);
-    public function setGeoCity(string $geoCity);
-    public function setGeoCountry(string $geoCountry);
-    public function setCanonicalUrl(string $canonicalUrl);
-    public function setSitemapUrl(string $sitemapUrl);
-    public function setFaviconUrl(string $faviconUrl);
-    public function setThemeColor(string $themeColor);
-    public function setSiteName(string $siteName);
-    public function setFbImageUrl(string $fbImageUrl);
-    public function setTwitterImageUrl(string $twitterImageUrl);
-    public function setWhatsappImageUrl(string $whatsappImageUrl);
-    public function addMeta(string $name, string $content);
-    public function render(): string;
-}
-
 class HeadGenerator implements HeadGeneratorInterface {
 
     protected string    $language = 'en';
